42ad669caf95b10fbf6c350da374f182454817a1
pavan <pavanmahesh0207@gmail.com>
2023-05-14 16:45:45 +0530

    weather app

---
+++ src/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "3b06fa7648msh17674031dac6bfep1d522fjsndf9604a1dffc",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "dbe7af1e2272b445645e477d043accbf";
